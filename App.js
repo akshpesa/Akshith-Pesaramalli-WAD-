323d998729c0aee7fc5d6ec8c894ef4ba21e8d51
akshpesa <122724899+akshpesa@users.noreply.github.com>
2024-06-21 16:07:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header';
+import RegistrationForm from './registrationForm';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <RegistrationForm />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} 22071A0502</p>
+    </footer>
+  );
+};
+
+export default Footer;
