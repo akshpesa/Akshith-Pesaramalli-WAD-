0896b4a6059737010a1b8c4cd2bc3915498239b6
akshpesa <122724899+akshpesa@users.noreply.github.com>
2024-06-21 16:15:07 +0530

    Update header.js

--- header.js
+++ header.js
@@ -1,11 +1,19 @@
 import React from 'react';
+
 function Header() {
-    return(
-        <nav class="bg-dark navbar-dark navbar">
-            <div className="row col-12 d-flex justify-content-center text-white">
-                <h3>Akshith Pesaramalli</h3>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="bg-dark navbar-dark navbar">
+      <div className="row col-12 d-flex justify-content-center text-white">
+        <h3>Akshith Pesaramalli</h3>
+        <div className="ml-auto">
+          <button className="btn btn-outline-light">About Us</button>
+          <button className="btn btn-outline-light ml-2">Login</button>
+          <button className="btn btn-outline-light ml-2">Shop</button>
+          <button className="btn btn-outline-light ml-2">Contact</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
+
 export default Header;
